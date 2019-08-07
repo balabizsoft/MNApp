0f9bbce7120de8f673c41baeda7e5964f352184f
bala <bala.bizsoft@gmail.com>
2019-08-07 12:32:13 +0800

    Add project files.

---
+++ MNApp.Lib/EMailExtract.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MNApp.Lib
+{
+    public class EMailExtract
+    {
+        private string currentDir;
+
+        public string CurrentDir
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(currentDir))   CurrentDir = Directory.GetCurrentDirectory();
+                
+                return currentDir;
+            }
+            set { currentDir = value; }
+        }
+
+
+        private string inputDir;
+
+        public string InputDir
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(inputDir))
+                {
+                    InputDir = $"{CurrentDir}/Input";
+                }
+                return inputDir;
+            }
+            set { inputDir = value; }
+        }
+
+        public bool InputDirExists
+        {
+            get
+            {                
+                return Directory.Exists(InputDir);
+            }
+        }
+
+        public string[] Files
+        {
+            get
+            {                
+                return Directory.GetFiles(InputDir, "*.txt", SearchOption.TopDirectoryOnly);
+            }
+        }
+        
+        public List<FileInfo> FileDetails
+        {
+            get
+            {
+                List<FileInfo> fileInfos = new List<FileInfo>();
+                foreach(string file in Files)
+                {
+                    fileInfos.Add(new FileInfo(file));
+                }
+                return fileInfos;
+            }
+        }
+    }
+}
---
+++ MNApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MNApp.Lib;
+
+namespace MNApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EMailExtract EME = new EMailExtract();
+
+            if (!EME.InputDirExists)
+            {
+                Console.WriteLine($"{EME.InputDir} does not exist.");
+                Console.ReadKey();
+                return;
+            }
+
+            foreach(var file in EME.FileDetails)
+            {
+                Console.WriteLine($"{file.Name} {file.LastWriteTime}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
